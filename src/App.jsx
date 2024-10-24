94d2b2280352c38fbdaabbc8eedc143656f6b720
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2024-10-24 19:42:26 +0530

    Added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { inject } from "@vercel/analytics";
 
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
@@ -10,6 +13,8 @@ import { AuthContext } from "./context/AuthContext";
 import "./styles/App.scss";
 import "./styles/LoginRegister.scss";
 import "./styles/HomePage.scss";
+
+inject();
 
 const App = () => {
   
@@ -23,27 +28,29 @@ const App = () => {
   };
 
   return (
-    <>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/">
-              <Route
-                index
-                element={
-                  <ProtectedRoute>
-                    <HomePage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="register" element={<RegisterPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
-  );
+		<>
+			<div className="App">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/">
+							<Route
+								index
+								element={
+									<ProtectedRoute>
+										<HomePage />
+									</ProtectedRoute>
+								}
+							/>
+							<Route path="login" element={<LoginPage />} />
+							<Route path="register" element={<RegisterPage />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+				<SpeedInsights />
+				<Analytics />
+			</div>
+		</>
+	);
 };
 
 export default App;
